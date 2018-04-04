851348b4d30aa25d6437bff052ba20f040a44df0
WANGYUNING <247197442@qq.com>
2018-04-04 09:39:56 +0800

    系统管理通讯录

--- newobject/myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ newobject/myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -79,6 +80,39 @@ public class MainActivity extends AppCompatActivity {
                 cValue.put("ADDRESS", "Hebei Normal University");
                 cValue.put("SALARY", 3200.0);
                 Uri uri1  = contentResolver.insert(uri,cValue);
+
+            }
+        });
+        Button button3 = findViewById(R.id.btn_4);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,null,
+                        null,null,null);
+                if (cursor.moveToFirst()){
+                    do {
+                        //通讯录名字
+                        int indexName = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                        String name = cursor.getString(indexName);
+                        //id
+                        int indexId = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                        int id = cursor.getInt(indexId);
+                        //根据id获得tel
+                        Cursor cursor1 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+                                        +"="+ContactsContract.Contacts._ID,null,
+                                null);
+                        if (cursor1.moveToFirst()){
+                            do {
+                                String tel = cursor1.getString(cursor1.getColumnIndex(
+                                        ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                Log.e("通讯录","name:"+name+" | "+"id:"+id+"|"+"tel:"+tel);
+                            }while (cursor1.moveToFirst());
+                        }
+
+                    }while (cursor.moveToFirst());
+                }
 
             }
         });
