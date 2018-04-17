158319534e7d80bcce1a9dfa16167a8d2d6a613d
WANGYUNING <247197442@qq.com>
2018-04-17 16:58:02 +0800

    baidumap

---
+++ baidumap/app/src/main/java/com/example/wang/baidumap/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.wang.baidumap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+    private MapView aMapView = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        // 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        // 注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        // 获取地图控件引用
+        aMapView = (MapView) findViewById(R.id.bmapView);
+
+    }
+}
